f8b9d2d9cab298c5a1b63b2b45f7b67fa8500a19
JaimeIngenia <jamoncayop@unal.edu.co>
2025-08-07 14:34:20 -0500

    Todo funcionando dejarlo quietico jaja

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 def configurar_log():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -7,3 +8,16 @@ def guardar_datos_csv(data, archivo):
     # Código para guardar datos en CSV
     pass
 
+def generar_nombre_archivo(base_path):
+    """
+    Genera un nombre de archivo único añadiendo un número al final si el archivo ya existe.
+    """
+    if not os.path.exists(base_path):
+        return base_path
+    else:
+        i = 1
+        while True:
+            nuevo_archivo = base_path.replace(".csv", f"_{i}.csv")
+            if not os.path.exists(nuevo_archivo):
+                return nuevo_archivo
+            i += 1
